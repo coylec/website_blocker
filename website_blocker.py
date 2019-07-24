38f6a2c17faf70e72d9b53bd09268e5a7aecb28e
Conrad Coyle <coylec@users.noreply.github.com>
2019-07-24 16:30:22 +0100

    add loop for removing websites

--- website_blocker.py
+++ website_blocker.py
@@ -8,7 +8,7 @@ website_list=["www.facebook.com", "facebook.com"]
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
-        print("i")
+        print("Work Hours")
         with open(hosts_temp, 'r+') as file:
             content = file.read()
             for website in website_list:
@@ -17,5 +17,12 @@ while True:
                 else:
                     file.write(redirect+" "+ website+"\n")
     else:
-        print("j")
+        with open(hosts_temp, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Personal Time")
     time.sleep(5)
