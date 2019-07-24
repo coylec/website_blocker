be5a23c710615ad74a1aa55844da6560030d8cf8
Conrad Coyle <coylec@users.noreply.github.com>
2019-07-24 13:07:26 +0100

    add conditional loop for adding websites

--- website_blocker.py
+++ website_blocker.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime as dt
 
+hosts_temp="hosts"
 hosts_path="/private/etc/hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com", "facebook.com"]
@@ -8,6 +9,13 @@ website_list=["www.facebook.com", "facebook.com"]
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("i")
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
     else:
         print("j")
     time.sleep(5)
